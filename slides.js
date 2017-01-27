aedf40a457002a2fb8337e5018bb22cc21e476fa
Geoff Villeneuve <gville.01@gmail.com>
2017-01-27 14:54:44 -0500

    project dump
    Signed-off-by: Geoff Villeneuve <gville.01@gmail.com>

---
+++ slides.js
@@ -0,0 +1,120 @@
+var slides = [{
+		  "image": "im1.jpg",
+		  "title": "Two Infants Treated with Universal Immune Cells Have Their Cancer Vanish",
+		  "description": "In a medical first, the children were treated with genetically engineered T-cells from another person.",
+		  "link": "https://www.technologyreview.com/s/603502/two-infants-treated-with-universal-immune-cells-have-their-cancer-vanish/"
+		}, 
+		{
+		  "image": "uber.jpg",
+		  "title": "Uber Takes Majority of Ground Transport Market for U.S. Business Travelers",
+		  "description": "Uber accounted for 52 percent of ground transportation business transactions last quarter on Certify.",
+		  "link": "https://www.bloomberg.com/news/articles/2017-01-26/uber-takes-majority-of-ground-transport-market-for-u-s-business-travelers"
+		}, 
+		{
+			"image": "china.jpg",
+		  "title": "China girlfriend rental app gets leg up from Lunar New Year demand.",
+		  "description": "As millions in China head home to celebrate the Lunar New Year holiday with their families, 24-year-old Luoluo is busy answering messages on a mobile app from desperate men looking to hire an instant girlfriend whom they can present to mom and dad.",
+		  "link": "http://www.reuters.com/article/us-lunar-newyear-china-girlfriend-idUSKBN15A1NY"
+		},
+		{
+			"image": "space.jpg",
+		  "title": "Boeing just unveiled its slick new 'Starliner' spacesuit to protect a new generation of NASA astronauts",
+		  "description": "Boeing for the first time publicly unveiled a crucial new piece of its Starliner system: A spacesuit to protect the next generation of astronauts as they launch toward the International Space Station, and eventually more exotic destinations like an asteroid or even Mars.",
+		  "link": "http://www.businessinsider.com/boeing-spacesuit-nasa-photos-2017-1/#this-is-the-new-cst-100-starliner-spacesuit-that-was-designed-and-built-by-boeing-1"
+		},
+		{
+			"image": "samsung.jpg",
+		  "title": "Samsung trademarks feature that would turn Galaxy S8 into a computer",
+		  "description": "This news is further corroborated by a report from the Guardian, which mentions a new \"DeX\" dock that will turn the phone into the brains for a computer. That dock will enable users to connect a mouse, keyboard, and other peripherals to the phone.",
+		  "link": "http://www.digitaltrends.com/mobile/samsung-galaxy-s8-news/"
+		},
+		{
+			"image": "speech.jpg",
+		  "title": "This app will save you from saying 'like' over and over at your next interview",
+		  "description": "The newly redesigned and aptly named LikeSo helps users drop filler words from their vocabulary, including \"like,\" \"so,\" \"actually\" and \"whatever,\" by tracking just how often they're used in speech.",
+		  "link": "http://www.cnbc.com/2017/01/26/app-will-save-you-from-saying-like-too-much-at-your-next-interview.html"
+		},
+		{
+			"image": "tesla.png",
+		  "title": "Tesla quietly brings online its massive - biggest in the world - 80 MWh Powerpack station with Southern California Edison",
+		  "description": "With a capacity of 20 MW/80 MWh, the project can hold enough energy to power more than 2,500 households for a day, but that's not really what Southern California Edison is using it for on its grid covering 15 million people.",
+		  "link": "https://electrek.co/2017/01/23/tesla-mira-loma-powerpack-station-southern-california-edison/"
+		},
+		{
+			"image": "laser.jpg",
+		  "title": "Brits, Czechs claim world's most powerful 'super laser'",
+		  "description": "A team of British and Czech scientists on Tuesday said they had successfully tested a \"super laser\" they claim is 10 times more powerful than any other of its kind on the planet.",
+		  "link": "http://www.france24.com/en/20170124-brits-czechs-claim-worlds-most-powerful-super-laser"
+		},
+		{
+			"image": "tostitos.jpg",
+		  "title": "Tostitos' New Party Bag Knows When You've Been Drinking and Will Even Call You an Uber",
+		  "description": "The special bag, created by Goodby Silverstein & Partners, comes equipped with a sensor connected to a microcontroller calibrated to detect small traces of alcohol on a person's breath. ",
+		  "link": "http://www.adweek.com/adfreak/tostitos-new-party-bag-knows-when-youve-been-drinking-and-will-even-call-you-uber-175727"
+		},
+		{
+			"image": "alexa.jpg",
+		  "title": "Amazon pushes Star Trek future with new Alexa wake word",
+		  "description": "The wake word \"computer.\" That's right, you can now pretend you're in the Star Trek universe.",
+		  "link": "https://www.engadget.com/2017/01/24/amazon-pushes-star-trek-future-with-new-alexa-wake-word/"
+		},
+		{
+			"image": "lock.jpg",
+		  "title": "Researchers propose a way to use your heartbeat as a password",
+		  "description": "Researchers at Binghamton State University in New York think your heart could be the key to your personal data. By measuring the electrical activity of the heart, researchers say they can encrypt patients' health records. ",
+		  "link": "http://www.javaworld.com/article/3161190/security/researchers-propose-a-way-to-use-your-heartbeat-as-a-password.html#tk.rss_all"
+		},
+		{
+			"image": "brainwaves.jpg",
+		  "title": "Brainwaves could act as your password - but not if you're drunk",
+		  "description": "Brainwave authentication is one of many biometric measures touted as an alternative to passwords. The idea is for a person to authenticate their identity with electroencephalogram (EEG) readings. ",
+		  "link": "https://www.newscientist.com/article/2118434-brainwaves-could-act-as-your-password-but-not-if-youre-drunk/?utm_source=MIT%20TR%20Newsletters&utm_campaign=8f37b32526-The_Download&utm_medium=email&utm_term=0_997ed6f472-8f37b32526-154304641"
+		},
+		{
+			"image": "skin_printer.jpg",
+		  "title": "Human skin can now be printed out in transplant breakthrough",
+		  "description": "A team of biologists has built a prototype 3D bioprinter that can create functional human skin, which could help bring an end to animal testing once and for all.",
+		  "link": "http://www.independent.co.uk/life-style/gadgets-and-tech/news/human-skin-3d-printing-transplants-chemical-testing-a7543616.html"
+		},
+		{
+			"image": "botnet.jpg",
+		  "title": "Massive Star Wars Twitter Botnet Uncovered",
+		  "description": "UK analysts accidentally uncovered more than 350,000 bogus accounts used to fabricate follower numbers, send spam, and boost interest in trending topics.",
+		  "link": "http://www.pcmag.com/news/351285/massive-star-wars-twitter-botnet-uncovered"
+		},
+		{
+			"image": "passport.jpg",
+		  "title": "Airport Immigration Check Using Facial Recognition? Passports To Be Replaced With New Technology In Australia By 2019-2020",
+		  "description": "Australia's international airports are in the process of automating 90 percent of air traveler processing by 2020 by implementing facial recognition technology that involves biometric recognition of faces, irises and/or fingerprints, hence eliminating the need to carry essential traveling documents such as passports.",
+		  "link": "http://www.ibtimes.com/airport-immigration-check-using-facial-recognition-passports-be-replaced-new-2479178"
+		},
+		{
+			"image": "india.jpg",
+		  "title": "India now has 462 million internet users - 79% of traffic is mobile",
+		  "description": "A little over a third of India's 1.3 billion people are connected to the internet. That's less than the global ratio: half of the world's 7.5 billion people are online. But in terms of sheer numbers of internet users, only China, with 731 million, has more than India's 462 million.",
+		  "link": "https://www.techinasia.com/india-462-million-internet-users-79-traffic-mobile"
+		},
+		{
+			"image": "seagate.jpg",
+		  "title": "Seagate will bring a 16TB hard drive to market within 18 months",
+		  "description": "Cloud-based storage is a popular alternative to maintaining local data backups or as part of a multi-location / off-site strategy, especially if you're dealing with mission-critical data that's irreplaceable. ",
+		  "link": "http://www.techspot.com/news/67928-seagate-bring-16tb-hard-drive-market-within-18.html"
+		},
+		{
+			"image": "robot.jpeg",
+		  "title": "Hydraulic muscle makes for tougher, stronger disaster-site robots",
+		  "description": "The idea of using robots as the go-to for handling disaster situations isn't new, but part of the problem has been how to build robots light enough to move about easily, yet are strong enough and tough enough to handle things like a smashed up nuclear reactor.",
+		  "link": "http://newatlas.com/hydraulic-muscle-robots/47597/"
+		},
+		{
+			"image": "fusion.jpg",
+		  "title": "Canada could be a world leader in nuclear fusion by 2030, report suggests",
+		  "description": "Scientists are calling on the federal government to invest more in fusion research as a way to produce massive amounts of clean energy and radically reduce dependance on fossil fuels.",
+		  "link": "http://www.cbc.ca/news/canada/british-columbia/canada-nuclear-fusion-2030-1.3953187"
+		},
+		{
+			"image": "trump.jpg",
+		  "title": "Donald Trump wants to 'close up' the Internet",
+		  "description": "In a speech at the U.S.S. Yorktown in Mount Pleasant, South Carolina, on Monday, Trump referenced the use by ISIS of social media as a recruitment tool. He recommended a discussion with Bill Gates to shut off parts of the Internet.",
+		  "link": "http://money.cnn.com/2015/12/08/technology/donald-trump-internet/"
+		}]
